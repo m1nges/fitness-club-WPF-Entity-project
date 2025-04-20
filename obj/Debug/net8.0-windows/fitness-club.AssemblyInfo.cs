9d53aaed46d606d51f86badd93092abf8a42a22a
Egor <efimovegor558@gmail.com>
2025-04-20 21:04:43 +0300

    Adding ClientPaymentPage

--- obj/Debug/net8.0-windows/fitness-club.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/fitness-club.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("fitness-club")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a15290e065dbd88c93a36663ba77299666a764be")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+66768f502022ec0ad815035c09955d75dc6854bb")]
 [assembly: System.Reflection.AssemblyProductAttribute("fitness-club")]
 [assembly: System.Reflection.AssemblyTitleAttribute("fitness-club")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
